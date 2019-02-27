2ef57bdf4927c02769b06d603a8e5757729eee6d
masterAndrey <filbert@yandex.ru>
2019-02-27 15:51:52 +0300

    async multiprocessing code refactoring

--- _includes/src/asyncio_multiprocessing.py
+++ _includes/src/asyncio_multiprocessing.py
@@ -4,7 +4,15 @@ import random
 
 semaphore = asyncio.Queue(maxsize=3-1)  # Max 3 processes
 
+
 async def worker(id):
+    """
+    We could use more straightforward consumer-producer pattern:
+        * producer puts tasks into the queue
+        * worker waits for tasks in the queue
+
+    But for this tiny code sniped that would produce too much boilerplates.
+    """
     delay = random.random()
     print('>'*5, f'task {id} starts with delay {delay:.1} seconds')
     process = await asyncio.create_subprocess_exec(
@@ -19,11 +27,13 @@ async def worker(id):
     print(f'Stdout: {output}, Stderr: {err}')
     await semaphore.get()
 
-async def main():
+
+async def main(loop):
     for task_id in range(6):
-        await semaphore.put(loop.create_task(worker(task_id)))
+        await semaphore.put(task_id)  # It does'n matter what we put in the queue. We use it as semaphore.
+        loop.create_task(worker(task_id))
+    # all the tasks are scheduled at the moment but not all done
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-# wait for all tasks in the loop
-loop.run_until_complete(asyncio.gather(*asyncio.Task.all_tasks()))
+loop.run_until_complete(main(loop))
+loop.run_until_complete(asyncio.gather(*asyncio.Task.all_tasks()))  # Wait for all tasks in the loop.
